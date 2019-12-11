67943c706278530f45b3ccd2ccf42705c80c94d7
Emil Gräs <emgr02002@utb.vaxjo.se>
2019-12-11 16:32:59 +0100

    En till klass

--- Classer/Classer/Program.cs
+++ Classer/Classer/Program.cs
@@ -4,19 +4,27 @@ namespace Classer
 {
     class Program
     {
-        public static void Main() {
-            
-                Fordon f = new Fordon();
-                f.SetNamn("Audi A6");
-                f.SetÅlder(2018);
-                f.SetTillvärkare("Volkswagen");
-                f.SetVikt(1760);
+        public static void Main()
+        {
+
+            Fordon f = new Fordon();
+            f.SetNamn("Audi A6");
+            f.SetÅlder(2018);
+            f.SetTillvärkare("Volkswagen");
+            f.SetVikt(1760);
 
             Console.WriteLine(f.GetNamn());
             Console.WriteLine(f.GetTillvärkare());
             Console.WriteLine(f.GetÅlder());
             Console.WriteLine(f.GetVikt());
-            }
+
+
+            Fordon t = new Fordon();
+            t.SetNamn("traktor");
+            t.SetÅlder();
+            t.
+
+        }
 
     }
 }
