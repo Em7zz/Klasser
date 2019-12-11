217f2aaa92db2cd00c60c469d0e9aeb9082fad4a
Emil Gräs <emgr02002@utb.vaxjo.se>
2019-12-11 16:22:32 +0100

    ignore

---
+++ Classer/Classer/Fordon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classer
+{
+    class Fordon
+    {
+        private string namn;
+        private int årsModel;
+        private string tillvärkare;
+        private int vikt;
+
+        public void SetNamn(string n) { namn = n; }
+
+        public string GetNamn() { return namn; }
+
+        public void SetÅlder(int å) { årsModel = å; }
+
+        public int GetÅlder() { return årsModel; }
+
+        public void SetTillvärkare(string t) { tillvärkare = t; }
+
+        public string GetTillvärkare() { return tillvärkare; }
+
+        public void SetVikt(int v) { vikt = v; }
+
+        public int GetVikt() { return vikt; }
+    }
+}
---
+++ Classer/Classer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classer
+{
+    class Program
+    {
+        public static void Main() {
+            
+                Fordon f = new Fordon();
+                f.SetNamn("Audi A6");
+                f.SetÅlder(2018);
+                f.SetTillvärkare("Volkswagen");
+                f.SetVikt(1760);
+
+            Console.WriteLine(f.GetNamn());
+            Console.WriteLine(f.GetTillvärkare());
+            Console.WriteLine(f.GetÅlder());
+            Console.WriteLine(f.GetVikt());
+            }
+
+    }
+}
+
