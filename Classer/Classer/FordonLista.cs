5cd86e7fc907d609f6bbe5fbaf5e83c8ef31cfa1
Emil Gräs <emgr02002@utb.vaxjo.se>
2020-01-15 16:34:30 +0100

    ll

---
+++ Classer/Classer/FordonLista.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classer
+{
+    class FordonsLista
+    {
+        List<Fordon> FordonLista = new List<Fordon>();
+    
+
+   }
+}
--- Classer/Classer/Program.cs
+++ Classer/Classer/Program.cs
@@ -9,36 +9,22 @@ namespace Classer
 
         public static void Main()
         {
-            List<Fordon> fordonsLista = new List<Fordon>();
-
-
+            List<Fordon> FordonsLista = new List<Fordon>();
 
             Fordon f = new Fordon("R8", 2019, "Audi", 850 );
             Console.WriteLine(f.Namn);
             Console.WriteLine(f.ÅrsModel);
             Console.WriteLine(f.Tillvärkare);
             Console.WriteLine(f.Vikt);
+            FordonsLista.Add(new Fordon("R8", 2019, "Audi", 850));
 
-            Console.WriteLine("   ");
-            
+            Console.WriteLine("   ");      
                        
             Fordon b = new Fordon("M5", 2019, "BMW", 750);
             Console.WriteLine(b.Namn);
             Console.WriteLine(b.ÅrsModel);
             Console.WriteLine(b.Tillvärkare);
             Console.WriteLine(b.Vikt);
-
-            
-
-
-           
-
-
-
-
-
-
-
         }
 
     }
